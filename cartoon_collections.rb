0413448f531bf4ff6771f4ff456212114ca1fbdb
Scott Matsuda <smatsuda2014@gmail.com>
2019-12-01 22:53:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  
+array.each_with_index { |item, index| puts "#{index + 1} + " " + #{item}" }
+  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  
+  # use collect to add the exclamation point, map to capitalize the first letter
+  (array.map { |i| i.capitalize }).collect { |x| x + "!" }
+  
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+ array.delete_if {|a| a.length <= 4}
+   if array.length > 0
+     return true
+    end
+  return false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  index = 0
+  while index < cheese_types.length do
+    
+    index += 1
+  end
+  p array
+  return array
 end
